a21803a6587e7f1b2797b17db56eccca761335c2
Pawanpreet31 <pawanpreetsingh10a216@gmail.com>
2024-08-02 09:26:57 -0400

    Adding image filenames and alternative text

--- assignment_4/part_2.js
+++ assignment_4/part_2.js
@@ -5,8 +5,16 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const imageArray = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
+const alternativeText = {
+    'pic1.jpg' : 'Eye',
+    'pic2.jpg' : 'Rock',
+    'pic3.jpg' : 'Flowers',
+    'pic4.jpg' : 'Egyptian Drawing',
+    'pic5.jpg' : 'Insect'
+}
 
 /* Looping through images */
 
